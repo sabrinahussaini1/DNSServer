86f13628bd7e312e9106de0b292138e3e8654531
sabrinahussaini1 <sh8448@nyu.edu>
2025-03-21 13:36:43 -0500

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -100,7 +100,7 @@ dns_records = {
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        dns.rdatatype.TXT: str(encrypted_value,),
+        dns.rdatatype.TXT: (str(encrypted_value),),
     }
 }
 
